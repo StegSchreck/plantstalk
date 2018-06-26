68bb516c3c87db517d0885d3f93d6cb504968776
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-06-26 19:34:51 +0200

    more cleanup

--- plantstalk.py
+++ plantstalk.py
@@ -2,17 +2,13 @@
 
 import signal
 
-import Adafruit_BMP.BMP085 as BMP085
 import Adafruit_DHT
 from influxdb import InfluxDBClient
 
 from RepeatedTimer import RepeatedTimer
 
-# BMP180 Sensor #
-bmp_sensor = BMP085.BMP085()
-
-# DHT22 Sensor #
-dht_sensor = Adafruit_DHT.DHT22
+# DHT11 Sensor #
+dht_sensor = Adafruit_DHT.DHT11
 gpio_input_pin = 19
 
 # InfluxDB #
@@ -25,7 +21,6 @@ json_body = [
         "fields": {
             "temperature": 0.0,
             "humidity": 0.0,
-            "pressure": 0.0,
         }
     }
 ]
@@ -33,15 +28,13 @@ json_body = [
 
 def measure():
     humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, gpio_input_pin)
-    pressure = bmp_sensor.read_pressure()
     if not humidity or not temperature:
         return measure()
-    return humidity, temperature, pressure
+    return humidity, temperature
 
 
-def send_measurements(client, humidity, temperature, pressure):
+def send_measurements(client, humidity, temperature):
     json_body[0]['fields']['temperature'] = temperature
-    json_body[0]['fields']['pressure'] = pressure
     if 0 <= humidity <= 100:
         json_body[0]['fields']['humidity'] = humidity
     else:
@@ -50,15 +43,15 @@ def send_measurements(client, humidity, temperature, pressure):
 
 
 def measurements(client):
-    humidity, temperature, pressure = measure()
-    send_measurements(client, humidity, temperature, pressure)
+    humidity, temperature = measure()
+    send_measurements(client, humidity, temperature)
 
 
 def main():
     client = InfluxDBClient(influx_host_ip, influx_host_port, influx_db)
     client.switch_database(influx_db)
 
-    measurements_timer = RepeatedTimer(10, measurements, client)
+    RepeatedTimer(10, measurements, client)
 
     while True:
         signal.pause()
