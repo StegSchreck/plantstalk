eb0557c016e9b364382f26e2428c9ff9094fbf92
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-08-11 15:58:03 +0200

    Add UV light sensor

--- plantstalk.py
+++ plantstalk.py
@@ -2,6 +2,8 @@
 
 import signal
 
+import smbus
+import veml6075
 import Adafruit_DHT
 from Adafruit_BMP.BMP085 import BMP085
 from gpiozero import InputDevice
@@ -16,6 +18,13 @@ gpio_input_pin_humidity_sensor = 5
 
 # BMP180 air pressure sensor #
 bmp_sensor = BMP085()
+
+# VEML 6075 UVA / UVB sensor
+bus4 = smbus.SMBus(4)
+uv_sensor = veml6075.VEML6075(i2c_dev=bus4)
+uv_sensor.set_shutdown(False)
+uv_sensor.set_high_dynamic_range(False)
+uv_sensor.set_integration_time('100ms')
 
 # InfluxDB #
 influx_host_ip = '127.0.0.1'
@@ -55,13 +64,33 @@ def measure_pressure():
     return pressure
 
 
-def send_measurements(client, humidity, temperature, pressure):
+def measure_uv_light():
+    uva, uvb = uv_sensor.get_measurements()
+    uv_comp1, uv_comp2 = uv_sensor.get_comparitor_readings()
+    uv_indices = uv_sensor.convert_to_index(uva, uvb, uv_comp1, uv_comp2)
+    uva_index = uv_indices[0]
+    uvb_index = uv_indices[1]
+    avg_uv_index = uv_indices[2]
+    print(uva)
+    if not uva:
+        return measure_uv_light()
+    return uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index
+
+
+def send_measurements(client, humidity, temperature, pressure, uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index):
     json_body[0]['fields']['temperature'] = temperature
     if 0 <= humidity <= 100:
         json_body[0]['fields']['humidity'] = float(humidity)
     else:
         json_body[0]['fields'].pop('humidity', 0)
     json_body[0]['fields']['pressure'] = pressure
+    json_body[0]['fields']['uva'] = uva
+    json_body[0]['fields']['uvb'] = uvb
+    json_body[0]['fields']['uv_comp1'] = uv_comp1
+    json_body[0]['fields']['uv_comp2'] = uv_comp2
+    json_body[0]['fields']['uva_index'] = uva_index
+    json_body[0]['fields']['uvb_index'] = uvb_index
+    json_body[0]['fields']['avg_uv_index'] = avg_uv_index
     client.write_points(json_body)
 
 
@@ -69,7 +98,8 @@ def measure(client):
     humidity = measure_humidity()
     temperature = measure_temperature()
     pressure = measure_pressure()
-    send_measurements(client, humidity, temperature, pressure)
+    uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index = measure_uv_light()
+    send_measurements(client, humidity, temperature, pressure, uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index)
 
 
 def main():
