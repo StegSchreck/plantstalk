1a4283709bc0e04565d2b8a326db8452f4fbc33a
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2020-07-29 21:24:22 +0200

    Update plantstalk.py for Terrarium

--- plantstalk.py
+++ plantstalk.py
@@ -6,16 +6,15 @@ import smbus
 import veml6075
 import Adafruit_DHT
 from Adafruit_BMP.BMP085 import BMP085
-from gpiozero import InputDevice
 from influxdb import InfluxDBClient
 
 import ds18b20 as temperature_sensor
-import GroveMultichannelGasSensor
 from RepeatedTimer import RepeatedTimer
 
 # DHT22 humidity sensor #
 dht_sensor = Adafruit_DHT.DHT22
-gpio_input_pin_humidity_sensor = 5
+gpio_input_pin_humidity_sensor_cold_spot = 5
+gpio_input_pin_humidity_sensor_hot_spot = 5 #TODO
 
 # BMP180 air pressure sensor #
 bmp_sensor = BMP085()
@@ -35,10 +34,12 @@ json_body = [
     {
         "measurement": influx_db,
         "fields": {
-            "temperature": 0.0,
-            "humidity": 0.0,
+            "cold_spot_humidity": 0.0,
+            "cold_spot_temperature": 0.0,
+            "hot_spot_humidity": 0.0,
+            "hot_spot_temperature": 0.0,
+            "room_temperature": 0.0,
             "pressure": 0.0,
-            "rain": 0.0,
             "uva": 0.0,
             "uvb": 0.0,
             "uv_comp1": 0.0,
@@ -51,18 +52,27 @@ json_body = [
 ]
 
 
-def measure_humidity():
-    humidity, _ = Adafruit_DHT.read_retry(dht_sensor, gpio_input_pin_humidity_sensor)
-    if not humidity:
-        return measure_humidity()
-    return humidity
+def measure_cold_spot():
+    return read_measurements_from_dht22(gpio_input_pin_humidity_sensor_cold_spot)
 
 
-def measure_temperature():
-    temperature = temperature_sensor.read()
-    if not temperature:
-        return measure_temperature()
-    return temperature
+def measure_hot_spot():
+    # return read_measurements_from_dht22(gpio_input_pin_humidity_sensor_hot_spot)
+    return -1, -1
+
+
+def read_measurements_from_dht22(gpio_pin):
+    humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, gpio_pin)
+    if not humidity or not temperature:
+        return read_measurements_from_dht22(gpio_pin)
+    return humidity, temperature
+
+
+def measure_room_temperature():
+    room_temperature = temperature_sensor.read()
+    if not room_temperature:
+        return measure_room_temperature()
+    return room_temperature
 
 
 def measure_pressure():
@@ -82,18 +92,28 @@ def measure_uv_light():
     return uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index
 
 
-def measure_toxic_gases():
-    gas_sensor = GroveMultichannelGasSensor.MultichannelGasSensor()
-    carbon_monoxide = gas_sensor.measure_CO()
-    print(carbon_monoxide)
-
-
-def send_measurements(client, humidity, temperature, pressure, uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index):
-    json_body[0]['fields']['temperature'] = temperature
-    if 0 <= humidity <= 100:
-        json_body[0]['fields']['humidity'] = float(humidity)
+def send_measurements(client, cold_spot_humidity, cold_spot_temperature, hot_spot_humidity, hot_spot_temperature, room_temperature, pressure, uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index):
+    if 0 <= cold_spot_humidity <= 100:
+        json_body[0]['fields']['cold_spot_humidity'] = float(cold_spot_humidity)
     else:
-        json_body[0]['fields'].pop('humidity', 0)
+        json_body[0]['fields'].pop('cold_spot_humidity', 0)
+
+    if 0 <= cold_spot_temperature <= 100:
+        json_body[0]['fields']['cold_spot_temperature'] = float(cold_spot_temperature)
+    else:
+        json_body[0]['fields'].pop('cold_spot_temperature', 0)
+
+    if 0 <= hot_spot_humidity <= 100:
+        json_body[0]['fields']['hot_spot_humidity'] = float(hot_spot_humidity)
+    else:
+        json_body[0]['fields'].pop('hot_spot_humidity', 0)
+
+    if 0 <= hot_spot_temperature <= 100:
+        json_body[0]['fields']['hot_spot_temperature'] = float(hot_spot_temperature)
+    else:
+        json_body[0]['fields'].pop('hot_spot_temperature', 0)
+
+    json_body[0]['fields']['room_temperature'] = room_temperature
     json_body[0]['fields']['pressure'] = pressure
     json_body[0]['fields']['uva'] = uva
     json_body[0]['fields']['uvb'] = uvb
@@ -106,11 +126,12 @@ def send_measurements(client, humidity, temperature, pressure, uva, uvb, uv_comp
 
 
 def measure(client):
-    humidity = measure_humidity()
-    temperature = measure_temperature()
+    cold_spot_humidity, cold_spot_temperature = measure_cold_spot()
+    hot_spot_humidity, hot_spot_temperature = measure_hot_spot()
+    room_temperature = measure_room_temperature()
     pressure = measure_pressure()
     uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index = measure_uv_light()
-    send_measurements(client, humidity, temperature, pressure, uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index)
+    send_measurements(client, cold_spot_humidity, cold_spot_temperature, hot_spot_humidity, hot_spot_temperature, room_temperature, pressure, uva, uvb, uv_comp1, uv_comp2, uva_index, uvb_index, avg_uv_index)
 
 
 def main():
@@ -119,7 +140,6 @@ def main():
     temperature_sensor.setup()
 
     RepeatedTimer(10, measure, client)
-    # measure_toxic_gases()
 
     while True:
         signal.pause()
